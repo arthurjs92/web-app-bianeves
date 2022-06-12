16f700380a1fdde47430c984b952e84b2ecd3af6
Arthur Santos <ajsantos5d@gmail.com>
2022-06-12 13:12:51 -0300

    swagger config complete

--- backend/marcenariacriativa/src/main/java/com/bialandingpage/marcenariacriativa/MarcenariacriativaApplication.java
+++ backend/marcenariacriativa/src/main/java/com/bialandingpage/marcenariacriativa/MarcenariacriativaApplication.java
@@ -3,13 +3,15 @@ package com.bialandingpage.marcenariacriativa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableSwagger2
 public class MarcenariacriativaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MarcenariacriativaApplication.class, args);
-		
-	}
 
+	}
 }
