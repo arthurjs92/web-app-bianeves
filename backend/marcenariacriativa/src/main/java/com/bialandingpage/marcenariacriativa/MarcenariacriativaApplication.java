7ff69e32ad0204ff1a2e3d7b456e441502e66c58
Arthur Santos <ajsantos5d@gmail.com>
2022-06-13 15:13:49 -0300

    method update final

--- backend/marcenariacriativa/src/main/java/com/bialandingpage/marcenariacriativa/MarcenariacriativaApplication.java
+++ backend/marcenariacriativa/src/main/java/com/bialandingpage/marcenariacriativa/MarcenariacriativaApplication.java
@@ -3,11 +3,10 @@ package com.bialandingpage.marcenariacriativa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+//@SpringBootApplication
 @EnableSwagger2
 public class MarcenariacriativaApplication {
 	public static void main(String[] args) {
