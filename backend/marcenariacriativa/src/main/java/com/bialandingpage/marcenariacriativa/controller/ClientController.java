985fa608521ee650b8299a7273582019d99e0c46
aluno <aluno@udicentro.local>
2022-06-27 22:26:55 -0300

    validation in home

--- backend/marcenariacriativa/src/main/java/com/bialandingpage/marcenariacriativa/controller/ClientController.java
+++ backend/marcenariacriativa/src/main/java/com/bialandingpage/marcenariacriativa/controller/ClientController.java
@@ -24,7 +24,7 @@ import com.bialandingpage.marcenariacriativa.model.Client;
 import com.bialandingpage.marcenariacriativa.service.ClientService;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/home")
 @CrossOrigin
 public class ClientController {
 
