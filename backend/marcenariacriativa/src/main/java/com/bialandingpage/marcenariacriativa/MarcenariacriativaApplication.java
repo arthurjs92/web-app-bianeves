d74b1c81e436fc7c69c5e6ad1a02aae6350a291b
patriciamsro <patriciamsro@gmail.com>
2022-07-06 11:35:51 -0300

    Fix de bug do tomcat em redirecionamento

--- backend/marcenariacriativa/src/main/java/com/bialandingpage/marcenariacriativa/MarcenariacriativaApplication.java
+++ backend/marcenariacriativa/src/main/java/com/bialandingpage/marcenariacriativa/MarcenariacriativaApplication.java
@@ -3,6 +3,7 @@ package com.bialandingpage.marcenariacriativa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -12,6 +13,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class MarcenariacriativaApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MarcenariacriativaApplication.class, args);
+	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MarcenariacriativaApplication.class);
 	}
 }
